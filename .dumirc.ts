57d5c78987214763608b99eb9c7962301188d806
Alicecici <2149382799@qq.com>
2024-04-26 16:03:58 +0800

    配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'dumi';
-
+const repo = 'my-blog'; // 项目名
 export default defineConfig({
-  themeConfig: {
+  title: 'my-blog',
+  mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+ themeConfig: {
     name: 'myBlog',
   },
 });
